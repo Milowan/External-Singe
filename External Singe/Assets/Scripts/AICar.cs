c6a1bc7682bfbd54d2a0c1dedae118fdf4e477ae
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-11-12 21:21:05 -0500

    working on aggressive AI pt1

--- External Singe/Assets/Scripts/AICar.cs
+++ External Singe/Assets/Scripts/AICar.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AICar : CarController
 {
+    public float range;
+
     // Update is called once per frame
     void Update()
     {
@@ -16,27 +18,50 @@ public class AICar : CarController
 
     private void Steer()
     {
-        Vector3 target = nextWaypoint.position - tf.position;
-        float angle = Vector3.SignedAngle(target, tf.forward, Vector3.up);
 
-        angle *= -1;
+        Quaternion leftAngle = Quaternion.AngleAxis(maxSteerAngle, Vector3.forward);
+        Quaternion rightAngle = Quaternion.AngleAxis(-maxSteerAngle, Vector3.forward);
+        RaycastHit leftHit;
+        RaycastHit rightHit;
+        bool hitL = (Physics.Raycast(tf.position, leftAngle.eulerAngles, out leftHit, range) && !(leftHit.collider.GetComponent<Terrain>()));
+        bool hitR = (Physics.Raycast(tf.position, rightAngle.eulerAngles, out rightHit, range) && !(rightHit.collider.GetComponent<Terrain>()));
+        Debug.DrawRay(tf.position, leftAngle.eulerAngles * range);
+        Debug.DrawRay(tf.position, rightAngle.eulerAngles * range);
 
-        if (angle > maxSteerAngle)
+        if (!hitL && !hitR)
         {
-            steerAngle = 1;
+            Vector3 target = nextWaypoint.position - tf.position;
+            float angle = Vector3.SignedAngle(target, tf.forward, Vector3.up);
+
+            angle *= -1;
+
+            if (angle > maxSteerAngle)
+            {
+                steerAngle = 1;
+            }
+            else if (angle < -maxSteerAngle)
+            {
+                steerAngle = -1;
+            }
+            else
+            {
+                steerAngle = angle / maxSteerAngle;
+            }
+
+            if (Mathf.Abs(steerAngle) < 0.4f)
+            {
+                steerAngle = 0;
+            }
         }
-        else if (angle < -maxSteerAngle)
+        else if (leftHit.collider != null)
         {
             steerAngle = -1;
         }
-        else
+        else if (rightHit.collider != null)
         {
-            steerAngle = angle / maxSteerAngle;
+            steerAngle = 1;
         }
 
-        if (Mathf.Abs(steerAngle) < 0.4f)
-        {
-            steerAngle = 0;
-        }
+        
     }
 }
